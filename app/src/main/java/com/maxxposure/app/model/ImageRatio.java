e810c3c371823f38e48cf695877876169ec0b6e4
sajid <mulla.sajid786@gmail.com>
2022-01-22 19:09:12 +0530

    Files Updated

---
+++ app/src/main/java/com/maxxposure/app/model/ImageRatio.java
@@ -0,0 +1,44 @@
+package com.maxxposure.app.model;
+
+public class ImageRatio {
+
+    private String imageRatioId;
+
+    private String imageRatio;
+
+    private String imageRatioCreatedAt;
+
+    private String imageRatioUpdatedAt;
+
+    public String getImageRatioId() {
+        return imageRatioId;
+    }
+
+    public void setImageRatioId(String imageRatioId) {
+        this.imageRatioId = imageRatioId;
+    }
+
+    public String getImageRatio() {
+        return imageRatio;
+    }
+
+    public void setImageRatio(String imageRatio) {
+        this.imageRatio = imageRatio;
+    }
+
+    public String getImageRatioCreatedAt() {
+        return imageRatioCreatedAt;
+    }
+
+    public void setImageRatioCreatedAt(String imageRatioCreatedAt) {
+        this.imageRatioCreatedAt = imageRatioCreatedAt;
+    }
+
+    public String getImageRatioUpdatedAt() {
+        return imageRatioUpdatedAt;
+    }
+
+    public void setImageRatioUpdatedAt(String imageRatioUpdatedAt) {
+        this.imageRatioUpdatedAt = imageRatioUpdatedAt;
+    }
+}
